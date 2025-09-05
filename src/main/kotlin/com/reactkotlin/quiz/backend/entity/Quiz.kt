9b19b490bdf6351bf8fd488d843a4fd280603c79
Oktay Rasimov <oktayrasimov123@gmail.com>
2025-09-06 01:28:44 +0300

    made quiz modal normal class,added counter to track id,changed variables to var for future update or put implementation

--- src/main/kotlin/com/reactkotlin/quiz/backend/entity/Quiz.kt
+++ src/main/kotlin/com/reactkotlin/quiz/backend/entity/Quiz.kt
@@ -1,10 +1,27 @@
 package com.reactkotlin.quiz.backend.entity
 
-data class Quiz(
+import com.reactkotlin.quiz.backend.dto.QuizReq
+import com.reactkotlin.quiz.backend.dto.QuizRes
+
+class Quiz(
     val id: Int,
-    val title: String,
-    val text: String,
-    val options: List<String>,
-    val answer: List<Int>
-)
+    var title: String,
+    var text: String,
+    var options: List<String>,
+    var answer: List<Int>
+){
+    companion object {
+        private var counter = 0
+
+        fun create(title: String, text: String, options: List<String>, answer: List<Int>): Quiz {
+            counter++
+            return Quiz(id = counter, title = title, text = text, options = options, answer = answer)
+        }
+    }
+}
+
+fun Quiz.toRes() = QuizRes(id,title, text, options)
+
+
+
 
