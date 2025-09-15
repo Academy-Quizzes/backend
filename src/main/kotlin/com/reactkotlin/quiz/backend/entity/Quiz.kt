285601833620e95cb87a06c72bfe1d47e877d78e
Oktay Rasimov <131280054+OktayRasimov@users.noreply.github.com>
2025-09-15 13:50:44 +0300

    SCRUM-11-create initial quiz model (#6)
    add initial quiz model with counter tracker,variables for spring security,test security config

---
+++ src/main/kotlin/com/reactkotlin/quiz/backend/entity/Quiz.kt
@@ -0,0 +1,25 @@
+package com.reactkotlin.quiz.backend.entity
+
+class Quiz(
+    val id: Int,
+    var title: String,
+    var text: String,
+    var options: List<String>,
+    var answers: List<Int>
+) {
+    companion object {
+        private var counter = 0
+
+        fun create(title: String, text: String, options: List<String>, answers: List<Int>): Quiz {
+            counter++
+            return Quiz(
+                id = counter,
+                title = title,
+                text = text,
+                options = options,
+                answers = answers
+            )
+        }
+    }
+
+}
