62f4bf69a420289b9e09ae749e325765efc08039
Oktay Rasimov <131280054+OktayRasimov@users.noreply.github.com>
2025-09-15 16:29:57 +0300

    SCRUM-31/SCRUM-26 add update functionality (#14)
    Implements quiz update functionality in QuizController and QuizServiceImpl. Key changes:
    - Added update endpoint in QuizController.kt to modify existing quizzes.
    - Updated QuizServiceImpl.kt with update logic, using QuizReq with answers field.
    - Ensured consistency with answers field across DTOs and mappers.
    
    Depends on SCRUM-30-25 (PR #13) - delete quiz implementation controller-service
    Resolves SCRUM-31: https://react-kotlin.atlassian.net/browse/SCRUM-31
    SCRUM-26: https://react-kotlin.atlassian.net/browse/SCRUM-26

--- src/main/kotlin/com/reactkotlin/quiz/backend/controller/QuizController.kt
+++ src/main/kotlin/com/reactkotlin/quiz/backend/controller/QuizController.kt
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -65,6 +66,20 @@ class QuizController(private val quizService: QuizService) {
             quizService.delete(quizId)
 
             ResponseEntity.noContent().build()
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+
+    @PutMapping("/quizzes/{id}")
+    fun updateQuiz(@PathVariable id: Int,@Valid @RequestBody quiz: QuizReq): ResponseEntity<Unit> {
+
+        return try {
+
+            quizService.update(id, quiz)
+            ResponseEntity.noContent().build()
+
         } catch (e: IllegalArgumentException) {
             ResponseEntity.notFound().build()
         }
